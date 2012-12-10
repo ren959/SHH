0b03c1f6faf60eb4143caabbadfcaa9a0e2b8de8
ren959 <ren959@126.com>
2012-12-10 20:31:09 +0800

    2

--- SSH/src/HELLO.java
+++ SSH/src/HELLO.java
@@ -7,6 +7,8 @@ public class HELLO {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("haha");
+System.out.println("hah2");
+System.out.println("haha");
 	}
 
 }
